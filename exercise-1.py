ffb27f5fd9e1a6274614539b0757b21c342e8879
EricNunes0 <90226385+EricNunes0@users.noreply.github.com>
2023-03-24 21:21:02 -0300

    RAD em Python - Exercício 1

---
+++ exercise-1.py
@@ -0,0 +1,27 @@
+proceed = True
+while proceed == True:
+    act = None
+    while act == None:
+        try:
+            act = int(input("1 - Adicionar linha\n2 - Ler linhas\n3 - Encerrar programa\n\nInforme sua próxima ação: "))
+        except:
+            act = None
+    if act == 1:
+        f = open("arquivo.txt", "w")
+        i = int(input("Quantas linhas deseja inserir? "))
+        for i in range(0, i):
+            q = str(input(f"{i + 1}º linha: "))
+            f.writelines(f"{q}\n")
+        f.close()
+    elif act == 2:
+        re = open("arquivo.txt", "r")
+        lines = len(re.readlines())
+        r = open("arquivo.txt", "r")
+        for i in range(0, lines):
+            conteudo = r.readline()
+            print(conteudo)
+    elif act == 3:
+        proceed = False
+        print("Programa encerrado!")
+    else:
+        print("❌ Ação inválida!")
