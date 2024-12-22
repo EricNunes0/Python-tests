252b5238aef1ac022bde159c677ea603737b350b
EricNunes0 <90226385+EricNunes0@users.noreply.github.com>
2024-12-22 18:08:28 -0300

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -22,6 +22,6 @@ while proceed == True:
             print(conteudo)
     elif act == 3:
         proceed = False
-        print("Programa encerrado!")
+        print("📴 Programa encerrado!")
     else:
         print("❌ Ação inválida!")
